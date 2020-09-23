79cd6fb072bbfc9fd20027076c4a74378ca6c5f4
seferzon <gitea@fake.local>
2020-09-23 19:00:04 +0330

    add passport to project

---
+++ database/seeders/PassportSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
+
+class PassportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Artisan::call('passport:keys');
+        Artisan::call('passport:client --name=SeferWebClient --no-interaction --password');
+    }
+}
